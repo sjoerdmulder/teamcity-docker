3a818d86087aeb83b78bb97d61790b1576b0ce11
Sjoerd Mulder <sjoerdmulder@gmail.com>
2016-07-22 12:01:43 +0200

    Fixing possible conflict with 2 versions when upgrading

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,10 @@ if [ ! -f "$TEAMCITY_DATA_PATH/lib/jdbc/postgresql-9.4.1209.jar" ];
 then
     echo "Downloading postgress JDBC driver..."
     wget -P $TEAMCITY_DATA_PATH/lib/jdbc https://jdbc.postgresql.org/download/postgresql-9.4.1209.jar
+    # Remove possible old one when upgrading...
+    rm -f $TEAMCITY_DATA_PATH/lib/jdbc/postgresql-9.3-1103.jdbc41.jar
 fi
+
 
 echo "Starting teamcity..."
 exec /opt/TeamCity/bin/teamcity-server.sh run
