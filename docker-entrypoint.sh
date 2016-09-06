c08b035a35e417048f56760fe3c2abec2f883ffa
root <root@docker-2-dev.jelastic.com>
2016-09-06 11:38:05 +0000

    Additional configuration for Postgres connection

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 POSTGRES_CURRENT_VERSION=9.4.1209
 POSTGRES_OLD_VERSION=9.3-1103.jdbc41
 
+: ${TEAMCITY_PORT:=8111}
+
 mkdir -p $TEAMCITY_DATA_PATH/lib/jdbc $TEAMCITY_DATA_PATH/config
 if [ ! -f "$TEAMCITY_DATA_PATH/lib/jdbc/postgresql-${POSTGRES_CURRENT_VERSION}.jar" ];
 then
@@ -12,6 +14,16 @@ then
     rm -f $TEAMCITY_DATA_PATH/lib/jdbc/postgresql-${POSTGRES_OLD_VERSION}.jar
 fi
 
+if [[ ! -z $POSTGRES_HOST && ! -f "$TEAMCITY_DATA_PATH/config/database.properties" ]];
+then
+    echo "Configuring connection to Postgres server..."
+    echo "connectionUrl=jdbc\:postgresql\://$POSTGRES_PORT_5432_TCP_ADDR\:$POSTGRES_PORT_5432_TCP_PORT/$POSTGRES_POSTGRES_DB" > $TEAMCITY_DATA_PATH/config/database.properties
+    echo "connectionProperties.user=$POSTGRES_POSTGRES_USER" >> $TEAMCITY_DATA_PATH/config/database.properties
+    echo "connectionProperties.password=$POSTGRES_POSTGRES_PASSWORD" >> $TEAMCITY_DATA_PATH/config/database.properties
+    echo "maxConnections=50" >> $TEAMCITY_DATA_PATH/config/database.properties    
+fi
+
+sed -i 's|Connector port=\"[0-9a-z.]\{1,\}\"|Connector port=\"${TEAMCITY_PORT}\"|g' /opt/TeamCity/conf/server.xml
 
 echo "Starting teamcity..."
 exec /opt/TeamCity/bin/teamcity-server.sh run
